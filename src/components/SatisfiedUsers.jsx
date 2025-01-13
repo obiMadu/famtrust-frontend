119ae2203a400498264fccba74fde0a0ba285569
Obi.M <mail@obimadu.pro>
2025-01-13 13:09:47 +0100

    Update SatisfiedUsers.jsx

--- src/components/SatisfiedUsers.jsx
+++ src/components/SatisfiedUsers.jsx
@@ -7,17 +7,17 @@ const SatisfiedUsers = () => {
             <h2 className='font-semibold' >15k+</h2> <p className='text-xs text-center md:text-lg'>Customer Service Points</p>
         </div>
         <div className=' w-[160px] h-[160px] flex flex-col items-center justify-center md:text-lg'>
-            <h2 className='font-semibold' >75</h2> <p className='text-xs text-center md:text-lg'>Countries Served</p>
+            <h2 className='font-semibold' >3</h2> <p className='text-xs text-center md:text-lg'>Countries Served</p>
         </div>
         <div className='bg-hover-bg ring-offset-8 ring-1 ring-slate-200 rounded-full  p-4 flex flex-col items-center text-white 
         justify-center md:rounded-full md:h-[140px] md:w-[140px] md:text-lg md:text-nowrap'>
-            <h2 className='font-semibold' >5m</h2> <p className='text-xs text-center md:text-lg'>Satisfied Users</p>
+            <h2 className='font-semibold' >1m+</h2> <p className='text-xs text-center md:text-lg'>Satisfied Users</p>
         </div>
         <div className='w-[160px] h-[160px] flex flex-col items-center justify-center text-center md:text-lg'>
-            <h2 className='font-semibold' >850+</h2> <p className='text-xs md:text-lg'>Financial Advisors</p>
+            <h2 className='font-semibold' >50+</h2> <p className='text-xs md:text-lg'>Financial Advisors</p>
         </div>
         <div className='w-[160px] h-[160px] flex flex-col items-center justify-center text-center md:text-lg'>
-            <h2 className='font-semibold' >300+</h2> <p className='text-xs md:text-lg '>Innovative Banking Solutions</p>
+            <h2 className='font-semibold' >10+</h2> <p className='text-xs md:text-lg '>Innovative Banking Solutions</p>
         </div>
     </div>
   )
